f8d05cfe3e43a5ce80d4b3b793ec0bd2531e4f21
Miguel <124186796+MiguelMendonSilva@users.noreply.github.com>
2025-03-18 19:56:12 -0300

    Update exerc1.c

--- exerc1.c
+++ exerc1.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
 
 int main() {
-    
    float nota, media, total = 0;
+   int fim = 3;
    int contador = 0;
    
-   while (contador < 4){
+   while (contador <= fim){
        printf("Digite a nota: ");
        scanf("%f", &nota);
        total= total + nota;
        contador = contador + 1;
    }
 
-    media = total / 3;
+    media = total / fim;
     printf("A media da nota é: %.2f", media);
   return 0;
 }  
